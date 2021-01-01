71ac9b6869c968c246bbaaa1d32b8db63e7e12ff
kyleperra <perra.kyle@gmail.com>
2020-12-31 19:37:05 -0800

    basic express server connected to webpack dist, react rendering to the page

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 // * THIS IS THE ENTRYPOINT FOR THE SERVER (NODEMON -- see package.json scripts 'start')
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+// this links our server to the dist folder, which will default to index.html when left like this:
+app.use(express.static('dist'));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
