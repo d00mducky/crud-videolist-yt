77d773eca4372c6572a45b738fb78d3db8e51d15
kyleperra <perra.kyle@gmail.com>
2021-01-02 20:13:54 -0800

    client fetch request for user search history (all previous yt search queries)

--- client/src/lib/getAllQueries.js
+++ client/src/lib/getAllQueries.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const getAllQueries = (callback) => {
 
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
 
+  fetch("http://localhost:3000/api/queries", requestOptions)
+    .then(response => response.text())
+    .then(result => callback(result))
+    .catch(error => console.log('error', error));
 };
 
 export default getAllQueries;
