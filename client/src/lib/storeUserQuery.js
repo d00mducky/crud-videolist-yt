0c3ef36c469c52251010b7f30651faf3b2be7c40
kyleperra <perra.kyle@gmail.com>
2021-01-03 02:27:37 -0800

    fetch command to store a user query to the search_history table

--- client/src/lib/storeUserQuery.js
+++ client/src/lib/storeUserQuery.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const storeUserQuery = () => {
+const storeUserQuery = (params, callback) => {
 
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("searchTerm", params[0]);
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+
+  fetch("http://localhost:3000/api/queries", requestOptions)
+    .then(response => response.text())
+    .then(result => callback(JSON.PARSE(result)))
+    .catch(error => console.log('error', error));
 }
 
 export default storeUserQuery;
