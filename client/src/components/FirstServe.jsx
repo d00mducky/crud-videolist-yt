8f939cba2899d950b9da6ef7da7d5c714e5b45a6
kyleperra <perra.kyle@gmail.com>
2021-01-02 20:29:31 -0800

    added todo

--- client/src/components/FirstServe.jsx
+++ client/src/components/FirstServe.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 const FirstServe = (props) => {
 
+  // TODO lets load these entries from our search_queries table
+
   return (
     <section className='first-serve-container'>
       <h1>Choose a path . . .</h1>
